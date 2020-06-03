8bc1a969e6339d95b3fcb6277e50ea65ee7e2754
Michael Melesse <micmelesse@gmail.com>
2020-06-03 02:30:02 +0000

    update package script

--- scripts/install_rocblas_packages.sh
+++ scripts/install_rocblas_packages.sh
@@ -1,6 +1,7 @@
-PKG_DIR=/dockerx/rocBLAS/rocblas_packages
+PKG_DIR=./packages
 
 # install packages
 # dpkg -i $PKG_DIR/rocblas-2.*ubuntu*.deb
-dpkg -i $PKG_DIR/bert-mi60-tuned.deb
 # dpkg -i $PKG_DIR/rocblas-clients*.deb
+# dpkg -i $PKG_DIR/bert-mi60-tuned.deb
+dpkg -i $PKG_DIR/bert-dlrm-rocblas-2.22.0.2349-dd0f634-ubuntu-16.04_amd64.deb
